c881f5fb22566c7b6a34841a7df09f7178a8bc81
Djimmy Poliard <poliarddjimmy@gmail.com>
2021-02-04 02:33:36 -0500

    Created new Screen

---
+++ src/screens/TermsConditionScreen.js
@@ -0,0 +1,9 @@
+
+import { WebView } from "react-native-webview";
+import React from "react";
+
+const TermsConditionScreen = () => {
+  return <WebView source={{ uri: "https://tiptoe.app/terms-condition" }} />
+}
+
+export default TermsConditionScreen;
