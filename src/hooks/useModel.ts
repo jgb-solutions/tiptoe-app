2bc18d70bbee3ed8044bc8a2265cd297896c39bc
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-25 05:02:54 -0400

    cleanup

--- src/hooks/useModel.ts
+++ src/hooks/useModel.ts
@@ -1,17 +1,5 @@
 import { useQuery } from "@apollo/react-hooks"
-
 import { FETCH_MODEL } from "../graphql/queries"
-// import { ApolloError } from "apollo-client"
-// import ModelInterface from "../interfaces/ModelInterface"
-
-// type Model = {
-// 	data: {
-// 		model: ModelInterface
-// 	}
-// 	loading: boolean
-// 	error: ApolloError | undefined
-// 	refetch: () => void
-// }
  
 export default function useModel(hash: string) {
 	const { loading, error, data, refetch } = useQuery(FETCH_MODEL, {
