079c36e43e8c11c50d4db79ba6041b0c56ac0487
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-26 12:33:49 -0400

    Updated useBillingData hook

--- src/hooks/useBillingData.ts
+++ src/hooks/useBillingData.ts
@@ -1,13 +1,14 @@
+import { ApolloError } from 'apollo-client';
 import { useQuery } from "@apollo/react-hooks"
 
 import { BILLING } from "../graphql/queries"
+import CardInterface from "../interfaces/CardInterface"
+import InvoiceInterface from "../interfaces/InvoiceInterface"
 
-export default function useBillingData() {
-	const {
-		data
-	} = useQuery(BILLING)
-
-	return { 
-		cards: data?.myCards
-	}
+export default function useBillingData(): {
+	data?: {  myCards: CardInterface[], myInvoices: InvoiceInterface[] },
+	loading: boolean,
+	error?: ApolloError
+}  {
+	return useQuery(BILLING)
 }
