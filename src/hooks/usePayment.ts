e811beefe660e2bd24c2a1340e23e1565c7dab1c
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-27 02:24:08 -0400

    format file

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -3,16 +3,9 @@ import { useQuery } from "@apollo/react-hooks"
 import { GETPUBLISHABLEKEY, CREATEPAYMENTINTENT } from "../graphql/queries"
 
 export default function usePayment() {
-	const {
-		data: key,
-	} = useQuery(GETPUBLISHABLEKEY) 
+  const { data } = useQuery(CREATEPAYMENTINTENT)
 
-	const {
-		data
-	} = useQuery(CREATEPAYMENTINTENT) 
-
-	return {
-		publishableKey: key,
-		intent: data.createPaymentIntent.client_secret
-	}
+  return {
+    intent: data.createPaymentIntent.client_secret,
+  }
 }
