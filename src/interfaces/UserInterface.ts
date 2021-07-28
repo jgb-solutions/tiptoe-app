df0599b1e082ffe2b419ac896f556dbcd5f3fe5b
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-28 01:23:03 -0400

    format document

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -1,19 +1,19 @@
 import ModelInterface from "./ModelInterface"
 
 export default interface UserInterface {
-	active: boolean
-	admin: boolean
-	avatar: string
-	email: string
-	first_login: boolean
-	id: string
-	created_at: Date
-	modele?: ModelInterface
-	modeles?: ModelInterface
-	name: string
-	telephone: string
-	user_type: string 
-	gender: string
-	updated_at: Date
-	pm_last_four: string
+  active: boolean
+  admin: boolean
+  avatar: string
+  email: string
+  first_login: boolean
+  id: string
+  created_at: Date
+  modele?: ModelInterface
+  modeles?: ModelInterface
+  name: string
+  telephone: string
+  user_type: string
+  gender: string
+  updated_at: Date
+  pm_last_four: string
 }
